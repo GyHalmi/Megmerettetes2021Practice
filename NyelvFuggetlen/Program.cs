c1f0bce9a1f0cb06d96667c7eceee45f3c95ba87
GyHalmi <georgehalmi@gmail.com>
2021-12-27 12:55:58 +0100

    Add project files.

---
+++ NyelvFuggetlen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NyelvFuggetlen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            round1();
+
+            Console.Read();
+        }
+
+        private static void round1()
+        {
+            
+        }
+    }
+}
